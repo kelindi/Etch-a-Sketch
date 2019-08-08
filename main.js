5591085556c7d78464f3e08a345d95b0f633b42e
Mr.Struggle <ayushk2020@gmail.com>
2019-08-08 14:16:37 -0700

    Added ability to clear the grid and resize

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 let draw = false
-
+const container = document.querySelector("#container")
 
 window.onload = function () {
-    const container = document.querySelector("#container")
-    createGrid(container);
-    
-    
-    
-    
+    createGrid(container,16);
+    createReset();
 }
 
-function createGrid(c) {
-    for (let index = 0; index < 256; index++) {
+function createGrid(c, n) {
+    for (let index = 0; index < n*n; index++) {
+        let dimension = (800/n) - 0.10 + 'px'; //calculate dimension of squares
+        //console.log(dimension);
         const sqr = document.createElement('div');
+        sqr.style.width = dimension; // set dimensions
+        sqr.style.height = dimension;
         sqr.classList.add('sqr');
         sqr.addEventListener('mouseover', () => {
             console.log(sqr);
             sqr.classList.add('sqrActive');
-    
         })
 
         c.appendChild(sqr);
@@ -25,3 +24,24 @@ function createGrid(c) {
 
 }
 
+function clearGrid(c) {
+    c.innerHTML = "";
+}
+
+
+function createReset(){
+    const reset = document.querySelector("#reset");
+    reset.addEventListener('click', () => {
+        clearGrid(container);
+        let d 
+        while (isNaN(d)){  //checks for valid input
+            d = askDimension();   
+        }
+        createGrid(container,d);
+    })
+}
+
+function askDimension(){
+    d = prompt("Please enter a grid dimension ex: 16 NOTE: Bigger the number the higher the wait time");
+    return d
+}
