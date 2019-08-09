5f14f9c5debefe9a86b5c275e59554440ee3a4df
Mr.Struggle <ayushk2020@gmail.com>
2019-08-08 21:54:41 -0700

    Added Title and changed Reset button to Clear Grid

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ let draw = false
 const container = document.querySelector("#container")
 
 window.onload = function () {
-    createGrid(container,16);
+    createGrid(container, 16);
     createReset();
 }
 
 function createGrid(c, n) {
-    for (let index = 0; index < n*n; index++) {
-        let dimension = (800/n) - 0.10 + 'px'; //calculate dimension of squares
+    for (let index = 0; index < n * n; index++) {
+        let dimension = (800 / n) - 0.10 + 'px'; //calculate dimension of squares
         //console.log(dimension);
         const sqr = document.createElement('div');
         sqr.style.width = dimension; // set dimensions
@@ -29,19 +29,19 @@ function clearGrid(c) {
 }
 
 
-function createReset(){
+function createReset() {
     const reset = document.querySelector("#reset");
     reset.addEventListener('click', () => {
         clearGrid(container);
-        let d 
-        while (isNaN(d)){  //checks for valid input
-            d = askDimension();   
+        let d
+        while (isNaN(d)) {  //checks for valid input
+            d = askDimension();
         }
-        createGrid(container,d);
+        createGrid(container, d);
     })
 }
 
-function askDimension(){
+function askDimension() {
     d = prompt("Please enter a grid dimension ex: 16 \nNOTE: Bigger the number the higher the wait time");
     return d
 }
